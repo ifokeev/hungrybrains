10794729d6962a2da1db00bcc73d161dc4cd3b41
Ivan Fokeev <vanya@vanya.cc>
2014-08-21 17:50:52 +0400

    Added custom errors pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root 'welcome#index'
+  match '/404', to: 'errors#file_not_found', via: :all
+  match '/422', to: 'errors#unprocessable', via: :all
+  match '/500', to: 'errors#internal_server_error', via: :all
+
 
   resources :users,    only: [:new, :create, :destroy]
   resources :sessions, only: [:new, :create, :destroy]
