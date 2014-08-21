6a79c53b13180e136ff5e9634988add912462841
Nikolay Bienko <bikolya@gmail.com>
2014-08-21 20:55:11 +0400

    Add simple new and create methods to Vacancies

--- app/controllers/vacancies_controller.rb
+++ app/controllers/vacancies_controller.rb
@@ -1,4 +1,5 @@
 class VacanciesController < ApplicationController
+  before_action :require_login, only: [:new, :create, :destroy]
 
 	def index
 		@vacancies = Vacancy.paginate(page: params[:page], per_page: 10)
@@ -7,5 +8,28 @@ class VacanciesController < ApplicationController
 	def show
 		@vacancy = Vacancy.find(params[:id])
 	end
+
+  def new
+    @vacancy = Vacancy.new
+  end
+
+  def create    
+    @vacancy = current_user.profile.vacancies.build(vacancy_params)
+    if @vacancy.save
+      redirect_to @vacancy, notice: "Success!"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def vacancy_params
+    params.require(:vacancy).permit(:title, :description, :brief_description)
+  end
+
+  def not_authenticated
+    redirect_to signin_path, alert: "Для просмотра это страницы необходимо авторизоваться"
+  end
 
 end
