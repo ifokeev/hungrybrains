54be49b8b1edade217c82b82a3def5f44c961cdc
Nikolay Bienko <bikolya@gmail.com>
2014-08-22 15:39:35 +0400

    Add show method to Profiles

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,4 +3,9 @@ class ProfilesController < ApplicationController
   def index    
   end
 
+  def show    
+    @profile   = Profile.find(params[:id])
+    @vacancies = @profile.vacancies
+  end
+
 end
