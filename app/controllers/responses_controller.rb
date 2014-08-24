cf3dec37486fd8339e6f3eebf66c2e0d9596778c
Nikolay Bienko <bikolya@gmail.com>
2014-08-24 22:38:53 +0400

    Add ajax to respond form

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -4,12 +4,18 @@ class ResponsesController < ApplicationController
   def create
     @vacancy = Vacancy.find(params[:response][:vacancy_id])
     current_user.student.respond(@vacancy)
-    redirect_to @vacancy
+    respond_to do |format|
+      format.html { redirect_to @vacancy }
+      format.js
+    end
   end
 
   def destroy
     @vacancy = Response.find(params[:id]).vacancy
     current_user.student.revoke(@vacancy)
-    redirect_to @vacancy    
+    respond_to do |format|
+      format.html { redirect_to @vacancy }
+      format.js
+    end
   end
 end
