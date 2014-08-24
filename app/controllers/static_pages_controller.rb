1406defaa113f0bb045173e2e1e01194b6134298
Nikolay Bienko <bikolya@gmail.com>
2014-08-24 17:43:32 +0400

    Add static pages

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,22 @@
+class StaticPagesController < ApplicationController
+
+  def index
+    if logged_in? and current_user.has_role? :student
+      redirect_to student_path(id: current_user.student.id)
+    elsif logged_in? and current_user.has_role? :company
+      redirect_to company_path(id: current_user.company.id)
+    else 
+       @vacancies = Vacancy.limit(5).order('id DESC')
+    end
+  end
+
+  def about    
+  end
+
+  def contact    
+  end
+
+  def feedback
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  root 'welcome#index'
   match '/404', to: 'errors#file_not_found', via: :all
   match '/422', to: 'errors#unprocessable', via: :all
   match '/500', to: 'errors#internal_server_error', via: :all
-
 
   resources :users,    only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
@@ -14,7 +12,11 @@ Rails.application.routes.draw do
       get :responses
     end
   end
-  match '/signup',  to: 'users#new',            via: 'get'
-  match '/signin',  to: 'sessions#new',         via: 'get'
-  match '/signout', to: 'sessions#destroy',     via: 'delete'
+  root 'static_pages#index'
+  match '/signup',   to: 'users#new',             via: 'get'
+  match '/signin',   to: 'sessions#new',          via: 'get'
+  match '/signout',  to: 'sessions#destroy',      via: 'delete'
+  match '/about',    to: 'static_pages#about',    via: 'get'
+  match '/contact',  to: 'static_pages#contact',  via: 'get'
+  match '/feedback', to: 'static_pages#feedback', via: 'get'
 end
