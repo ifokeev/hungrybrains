3981e7001d9280c8c2db3b60a8146284fa564c0c
Nikolay Bienko <bikolya@gmail.com>
2014-08-26 02:02:23 +0400

    Update views with avatars

--- app/models/company.rb
+++ app/models/company.rb
@@ -4,9 +4,13 @@ class Company < ActiveRecord::Base
   has_many   :relationships
   has_many   :students, through: :relationships, dependent: :destroy
 
-  has_attached_file :avatar, styles: { medium: "300x300>", 
-                                       thumb:  "100x100>" }, 
+  has_attached_file :avatar, styles: { medium: "200x200>", 
+                                       thumb:  "75x75>" }, 
                              default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
+  def avatar_from_url(url)
+    self.avatar = URI.parse(url)
+  end
 
 end
